80c3b8b5f02b271cf18556f54451fdfdc1ae7760
RianLimeira <rianlimeira@gmail.com>
2022-08-08 08:34:04 -0300

    calculadora IMC

--- app/src/main/java/com/example/calculatorimc/MainActivity.kt
+++ app/src/main/java/com/example/calculatorimc/MainActivity.kt
@@ -1,44 +1,36 @@
 package com.example.calculatorimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("lifecicle", "CREATE - estou CRIANDO a tela")
+        val alturas = findViewById<EditText>(R.id.altura)
+        val pesos = findViewById<EditText>(R.id.peso)
+        var resultado = findViewById<TextView>(R.id.resultadoIMC)
+        val btn = findViewById<Button>(R.id.button)
+
+
+        btn.setOnClickListener() {
+            if (pesos != null && alturas != null) {
+                resultado.visibility = View.VISIBLE
+                var restultIMC = pesos.text.toString().toFloat() / (alturas.text.toString()
+                    .toFloat() * alturas.text.toString().toFloat())
+                resultado.text = "Seu IMC = " + restultIMC
+            }
+        }
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("lifecicle", "START - estou VISIVEL a tela")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.w("lifecicle", "RESUME - estou INTRAGIVEL a tela")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("lifecicle", "PAUSE - estou PAUSADA a tela")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.w("lifecicle", "STOP - NÃO estou VISIVEL a tela, MAS FUNCIONAL")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("lifecicle", "RESTART - estou RECRIANDO a tela PAUSADA")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("lifecicle", "DESTROY - estou FECHANDO a tela")
-    }
 }
+
